a0565287d64a5785a85de3bdff4c0e0f9a270377
qianfeng001 <qianfeng001@xiabowen.local>
2015-06-18 15:41:23 +0800

    Initial Commit

---
+++ 找出一个字符串中出现次数最多的单词，并计数/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  找出一个字符串中出现次数最多的单词，并计数
+//
+//  Created by qianfeng001 on 15/6/18.
+//  Copyright (c) 2015年 伟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
